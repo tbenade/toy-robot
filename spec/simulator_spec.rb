79f04f84d9c22b60f4b32858d7b906deac5eef45
Terrence Benade <terrencebenade@gmail.com>
2014-07-17 16:26:02 +1000

    tidied the spec

--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -51,7 +51,8 @@ describe Simulator do
     end
 
     context 'intialized simulator with valid table and robot' do
-      subject(:simulator) {Simulator.new(table, robot)}
+      let(:output_stream){double("output_stream", :puts => nil)}
+      subject(:simulator) {Simulator.new(table, robot, output_stream)}
 
       it 'successful place' do
         expect(simulator.execute_command("place 0,0,NORTH")).to be true
@@ -81,20 +82,6 @@ describe Simulator do
         simulator.execute_command("PLACE 0,0,NORTH")
         expect(simulator.execute_command("MOVE")).to be true
       end
-    end
-
-    context 'robot placed' do
-      let(:output_stream){double("output_stream", :puts => nil)}
-      subject(:simulator) {
-        simulator =  Simulator.new(table, robot, output_stream)
-        simulator.execute_command("PLACE 0,0,EAST")
-        simulator
-      }
-
-      it 'reports location when asked' do
-        expect(output_stream).to receive(:puts)
-        simulator.execute_command("REPORT")
-      end
 
       it 'performs scenario A of specification' do
         commands = ['PLACE 0,0,NORTH','MOVE','REPORT']
@@ -122,7 +109,20 @@ describe Simulator do
         expect(output_stream).to receive(:puts).with("3,3,NORTH")
         simulator.execute_command('REPORT')
       end
+    end
 
+    context 'robot placed' do
+      let(:output_stream){double("output_stream", :puts => nil)}
+      subject(:simulator) {
+        simulator =  Simulator.new(table, robot, output_stream)
+        simulator.execute_command("PLACE 0,0,EAST")
+        simulator
+      }
+
+      it 'reports location when asked' do
+        expect(output_stream).to receive(:puts)
+        simulator.execute_command("REPORT")
+      end
     end
   end
 end
